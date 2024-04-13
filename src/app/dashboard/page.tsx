432d086f84db3ba759be82b6e23b701ad58d7fdf
S.Bondarchuk@niaepnn.ru <164788523+micozevs@users.noreply.github.com>
2024-04-13 17:55:52 +0300

    commit in hookah

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { Typography, Layout } from 'antd'
+import { NextPage } from 'next'
+
+const { Header, Content } = Layout
+const { Title } = Typography
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return (<>
+    <Content>
+    </Content>
+  </>)
+}
+
+export default Page
---
+++ src/components/Sider.tsx
@@ -0,0 +1,21 @@
+
+import React, { FC, useState } from 'react'
+import { Layout, Menu, Button, theme } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import Link from 'next/link';
+import Title from 'antd/es/typography/Title';
+import Sider from 'antd/es/layout/Sider';
+import { Content, Header } from 'antd/es/layout/layout';
+
+interface Props {}
+
+const SiderCom:FC<Props> = ({ }) => {
+    // const [collapsed, setCollapsed] = useState(true);
+    return (
+        <>
+       
+        </>
+    )
+}
+
+export default SiderCom
